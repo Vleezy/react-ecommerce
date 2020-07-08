dee0ee6ce920fb10f6821c73f4dda9097833241c
Vladimir Romero <vladdromero@gmail.com>
2020-07-08 00:37:52 -0400

    updating a security risk

--- src/components/Footers.js
+++ src/components/Footers.js
@@ -8,16 +8,16 @@ export default class Footers extends Component {
 
     <nav>
       <ul className="footer__area--icons">
-        <li><a href="http://www.facebook.com" target="_blank" rel="noopener"><img className="icon__facebook"
+        <li><a href="http://www.facebook.com" target="_blank" rel="noopener noreferrer"><img className="icon__facebook"
               src="/images/facebook.png" alt="Facebook" /></a></li>
 
-        <li><a href="http://www.twitter.com" target="_blank" rel="noopener"><img className="icon__twitter" src="/images/twitter.png"
+        <li><a href="http://www.twitter.com" target="_blank" rel="noopener noreferrer"><img className="icon__twitter" src="/images/twitter.png"
               alt="Twitter" /></a></li>
 
-        <li><a href="http://www.youtube.com" target="_blank" rel="noopener"><img className="icon__youtube" src="/images/youtube.png"
+        <li><a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer"><img className="icon__youtube" src="/images/youtube.png"
               alt="Youtube" /></a></li>
 
-        <li><a href="http://www.instagram.com" target="_blank" rel="noopener"><img className="icon__instagram"
+        <li><a href="http://www.instagram.com" target="_blank" rel="noopener noreferrer"><img className="icon__instagram"
               src="/images/instagram.png" alt="Instagram" /></a></li>
       </ul>
     </nav>
